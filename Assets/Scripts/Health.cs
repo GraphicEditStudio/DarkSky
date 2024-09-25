139abfe068ba82e2c7e6749309e24da6ebf79072
GraphicEdit <contact@graphicedit.com>
2024-09-25 16:19:36 +0100

    Stamina system and typo fix Health

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    //public GameObject m_deadScreen;
+    [Tooltip("Current Health")]
+    public float _currentHealth;
+    [Tooltip("Maximum Health")]
+    public float _maxHealth;
+    public Image _healthBar;
+    private Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+
+        slider.maxValue = health;
+        slider.value = health;
+
+    }
+
+    public void SetHealth(int health)
+    {
+
+        slider.value = health;
+
+    }
+
+    //HealthSystem and dead screen
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.gameObject.tag == "Enemy")
+        {
+            _currentHealth -= 25f;
+            _healthBar.fillAmount = _currentHealth / _maxHealth;
+
+            //Debug.Log("Was hit");
+        }
+
+        //if (_currentHealth <= 0) m_deadScreen.SetActive(true);
+
+    }
+
+}
